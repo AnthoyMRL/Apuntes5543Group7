3356599b4ba0e0c63059c07233bef3e5024ba0cd
AnthonyMRL <anthony.morales.velez@udla.edu.ec>
2025-06-25 23:54:33 -0500

    Creacion de la vista modelo del clima

---
+++ Apuntes5543Group7/ViewModels/WatherViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Apuntes5543Group7.Repositories;
+using static Apuntes5543Group7.Models.WeatherModels;
+
+namespace Apuntes5543Group7.ViewModels
+{
+    internal class WeatherViewModel : INotifyPropertyChanged
+    {
+        private WeatherData _weatherData = new WeatherData();
+
+        public WeatherData WeatherDataInfo
+        {
+            get => _weatherData;
+            set
+            {
+                if (_weatherData != value)
+                {
+                    _weatherData = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public WeatherViewModel()
+        {
+            GetCurrentWeatherData();
+        }
+
+        public async void GetCurrentWeatherData()
+        {
+            WeatherRepository weatherRepository = new WeatherRepository();
+            WeatherDataInfo = await weatherRepository.GetCurrentLocationWeatherData();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
